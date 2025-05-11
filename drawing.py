858fc8fe192eb3d920b0227d7c169be898d21552
Mateusz Anikiej <mateusz@anikiej.com>
2025-05-11 10:40:16 +0200

    drawing star

--- drawing.py
+++ drawing.py
@@ -2,11 +2,7 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
-pygame.init()
-
-# set up the window
 DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
-pygame.display.set_caption("Drawing")
 
 # set up the colors
 BLACK = (0, 0, 0)
@@ -15,30 +11,41 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# draw on the surface object
 DISPLAYSURF.fill(WHITE)
-pygame.draw.polygon(
-    DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106))
-)
-pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
-pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
-pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
-pygame.draw.ellipse(DISPLAYSURF, RED, (300, 200, 40, 80), 1)
-pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
 
-pixObj = pygame.PixelArray(DISPLAYSURF)
-pixObj[380][280] = BLACK
-pixObj[382][282] = BLACK
-pixObj[384][284] = BLACK
-pixObj[386][286] = BLACK
-pixObj[388][288] = BLACK
-del pixObj
 
-# run the game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+def draw_star():
+    star_points = [
+        (200, 20),  # top
+        (220, 110),
+        (310, 110),
+        (235, 165),
+        (255, 260),  # bottom right
+        (200, 200),
+        (145, 260),  # bottom left
+        (165, 165),
+        (90, 110),
+        (180, 110),
+    ]
+    pygame.draw.polygon(DISPLAYSURF, GREEN, star_points)
+
+
+def main():
+    pygame.init()
+
+    # set up the window
+    pygame.display.set_caption("Drawing")
+
+    draw_star()
+
+    # run the game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
