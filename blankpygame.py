b2ec3e3bec2142b3c137cb79d0a25b7c46423d86
Mateusz Anikiej <mateusz@anikiej.com>
2025-05-11 10:31:15 +0200

    Initial commit

---
+++ blankpygame.py
@@ -0,0 +1,20 @@
+import sys
+
+import pygame
+from pygame.locals import QUIT
+
+
+def main():
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((400, 300))
+    pygame.display.set_caption("Hello Pygame World!")
+
+    while True:  # main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
